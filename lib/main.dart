6e4357c1b8ec5efba9fc740072b24cf4be753f51
jadson santos <jadsoonbol@gmail.com>
2023-11-01 00:58:23 -0300

    login local

--- lib/main.dart
+++ lib/main.dart
@@ -1,14 +1,19 @@
 import 'package:flutter/material.dart';
 import 'package:my_bank_auth_local/home_page.dart';
-import 'package:firebase_core/firebase_core.dart';
-import 'firebase_options.dart';
+import 'package:my_bank_auth_local/login_page.dart';
+// import 'package:firebase_core/firebase_core.dart';
+// import 'firebase_options.dart';
 
-void main() async {
-  WidgetsFlutterBinding.ensureInitialized();
-  await Firebase.initializeApp(
-    options: DefaultFirebaseOptions.currentPlatform,
-  );
-  runApp(MyApp());
+void main() {
+  // WidgetsFlutterBinding.ensureInitialized();
+  // await Firebase.initializeApp(
+  //   options: DefaultFirebaseOptions.currentPlatform,
+  // );
+  runApp(const MyApp());
+  // final isValid = validate(email: 'email', password: 'password');
+  // if (isValid == null) {
+  //   login(email: 'email', password: 'password');
+  // }
 }
 
 class MyApp extends StatelessWidget {
@@ -23,7 +28,43 @@ class MyApp extends StatelessWidget {
         primarySwatch: Colors.blue,
         visualDensity: VisualDensity.adaptivePlatformDensity,
       ),
-      home: HomePage(),
+      initialRoute: '/login',
+      routes: {
+        '/login': (context) => const LoginPage(),
+        '/': (context) => const HomePage()
+      },
     );
   }
 }
+
+// login
+
+// void login({required email, required String? password}) {
+//   bool response = validateLogin(email: 'email', password: 'password');
+//   if (response) {
+//     print('abrindo a homePage');
+//   }
+// }
+
+// String? validate({String? email, String? password}) {
+//   final passwordIsValid = validatePassword(password);
+//   final emailIsValid = validateEmail(email);
+
+//   if (passwordIsValid == false) {
+//     return 'A senha precisa ter pelo menos seis digitos';
+//   }
+//   if (emailIsValid == false) {
+//     return 'O campo email é obrigatorio!';
+//   }
+
+//   return null;
+// }
+
+// bool validateEmail(String? email) => email != null && email.length > 0;
+// bool validatePassword(String? password) =>
+//     password != null && password.length > 6;
+
+// bool validateLogin({required String email, required String password}) {
+//   print('conectando ao servidor');
+//   return true;
+// }
